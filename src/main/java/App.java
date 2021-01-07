af0c8dba04ef6b33522b65980a243a3b2eb9a25d
Brian <bweloba@gmail.com>
2021-01-07 10:43:28 +0300

    Update favorite_photos.hbs and migrate code to it

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,20 +13,8 @@ public class App {
             return new ModelAndView(new HashMap(), "hello.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/favorite_photos", (request, response) ->
-                "<DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootsrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Travelling Photos</h1>" +
-                        "<ul>" +
-                        "<li><img src='/images/alex-quezada-VJWocACP39g-unsplash.jpg' alt='A photo of an Old building.'/></li>" +
-                        "<li><img src='/images/kristina-delp-LHetkULYo3I-unsplash.jpg' alt='A photo of a bird.'/></li>" +
-                        "</ul>" +
-                        "</body>" + "</html>"
-        );
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }

269f7ee778ed839da8e2483fe2deed57faf6532d
Brian <bweloba@gmail.com>
2021-01-07 10:29:54 +0300

    Remove letter html from java file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,31 +1,17 @@
 import static spark.Spark.*;
-import java.util.Map;
-import  spark.ModelAndView;
+
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Hello From Afar</h1>" +
-                        "<p>Dear Friend,</p>" +
-                        "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                        "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                        "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                        "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                        "<p>Cheers,</p>" +
-                        "<p>Travel Enthusiast Brian</p>" +
-                        "<p><a href='/favorite_photos' >P.S. Check out my favorite travel photos here.</a></p>" +
-                        "</body>" +
-                        "</html>"
-        );
+        get("/", (request, response) -> {
+            return new ModelAndView(new HashMap(), "hello.hbs");
+        }, new HandlebarsTemplateEngine());
 
         get("/favorite_photos", (request, response) ->
                 "<DOCTYPE html>" +
@@ -40,7 +26,7 @@ public class App {
                         "<li><img src='/images/alex-quezada-VJWocACP39g-unsplash.jpg' alt='A photo of an Old building.'/></li>" +
                         "<li><img src='/images/kristina-delp-LHetkULYo3I-unsplash.jpg' alt='A photo of a bird.'/></li>" +
                         "</ul>" +
-                        "</body>" +                        "</html>"
+                        "</body>" + "</html>"
         );
     }
 }

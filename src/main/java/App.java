4fc4739b1792bf706a2803be47ce24da02cf913a
Brian <bweloba@gmail.com>
2021-01-08 10:55:28 +0300

    fix typos

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import static spark.Spark.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -9,12 +10,14 @@ public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
 
-        get("/", (request, response) -> {
-            return new ModelAndView(new HashMap(), "hello.hbs");
-        }, new HandlebarsTemplateEngine());
+        get("/", (request, response) -> { //request for route happens at this location
+            Map<String, Object> model = new HashMap<String, Object>(); // new model is made to store information
+            return new ModelAndView(model, "hello.hbs"); // assemble individual pieces and render
+        }, new HandlebarsTemplateEngine()); //
 
         get("/favorite_photos", (request, response) -> {
-            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "favorite_photos.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
